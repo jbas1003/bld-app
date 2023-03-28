d88676fd308944a72b0f50f644401d1204f085d1
Jon <joncaesar@gmail.com>
2023-03-29 00:39:13 +0800

    Update Participants.jsx

--- src/views/Admin-dashboard/Participants/Participants.jsx
+++ src/views/Admin-dashboard/Participants/Participants.jsx
@@ -19,7 +19,7 @@ const Participants = () => {
                 </a>
             </li>
             <li class="mr-2">
-                <a href="#" class="inline-flex p-4 border-b-2 border-transparent rounded-t-lg hover:text-Red hover:border-gray-300 dark:hover:text-gray-300 group">
+                <a href="#" class="inline-flex p-4 border-b-2 border-transparent rounded-t-lg hover:text-Red hover:border-Red group">
                 <svg fill="none" stroke="currentColor" class="w-5 h-5 mr-2 text-gray-500 group-hover:text-Red" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"></path>
                 </svg>
@@ -27,7 +27,7 @@ const Participants = () => {
                 </a>
             </li>
             <li class="mr-2">
-                <a href="#" class="inline-flex p-4 border-b-2 border-transparent rounded-t-lg hover:text-Red hover:border-gray-300 dark:hover:text-gray-300 group">
+                <a href="#" class="inline-flex p-4 border-b-2 border-transparent rounded-t-lg hover:text-Red hover:border-Red group">
                 <svg fill="none" stroke="currentColor" class="w-5 h-5 mr-2 text-gray-500 group-hover:text-Red" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M3 3v1.5M3 21v-6m0 0l2.77-.693a9 9 0 016.208.682l.108.054a9 9 0 006.086.71l3.114-.732a48.524 48.524 0 01-.005-10.499l-3.11.732a9 9 0 01-6.085-.711l-.108-.054a9 9 0 00-6.208-.682L3 4.5M3 15V4.5"></path>
                 </svg>
