2a86269c8e0f4166e817f98dd39f05a58a7c9290
Jon <joncaesar@gmail.com>
2023-03-29 00:37:18 +0800

    Update TopNav.jsx

--- src/statics/Top-nav/TopNav.jsx
+++ src/statics/Top-nav/TopNav.jsx
@@ -28,27 +28,21 @@ const TopNav = () => {
                                     <img className="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user-photo"/>
                                 </button>
                             </div>
-                            <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
-                                <div className="px-4 py-3" role="none">
-                                    <p className="text-sm text-gray-900 dark:text-white" role="none">
+                            <div className="z-50 border border-solid border-gray-200 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow" id="dropdown-user">
+                                <div className="px-4 py-3 bg-Red" role="none">
+                                    <p className="text-sm font-semibold text-WhiteSmoke" role="none">
                                         Neil Sims
                                     </p>
-                                    <p className="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
+                                    <p className="text-sm font-semibold text-WhiteSmoke truncate" role="none">
                                         neil.sims@flowbite.com
                                     </p>
                                 </div>
                                 <ul className="py-1" role="none">
                                     <li>
-                                        <Link to=".." className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Dashboard</Link>
+                                        <Link to=".." className="block px-4 py-2 text-sm text-gray-700 hover:bg-Red hover:text-WhiteSmoke" role="menuitem">Settings</Link>
                                     </li>
                                     <li>
-                                        <Link to=".." className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Settings</Link>
-                                    </li>
-                                    <li>
-                                        <Link to=".." className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Earnings</Link>
-                                    </li>
-                                    <li>
-                                        <Link to=".." className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Sign out</Link>
+                                        <Link to=".." className="block px-4 py-2 text-sm text-gray-700 hover:bg-Red hover:text-WhiteSmoke" role="menuitem">Sign out</Link>
                                     </li>
                                 </ul>
                             </div>
