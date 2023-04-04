c4b1943670324c01a2b9954f5fd87222346a4192
Jon <joncaesar@gmail.com>
2023-04-05 07:04:46 +0800

    Update DTRegistration.jsx

--- src/views/Admin-dashboard/Participants/Registration/DTRegistration.jsx
+++ src/views/Admin-dashboard/Participants/Registration/DTRegistration.jsx
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { GetAllParticipants } from '../../../../utils/ParticipantsMethods';
 
 const DTRegistration = () => {
     const [participants, setParticipants] = useState();
 
     const getParticipants = () => {
-        GetAllParticipants()
-        .then(result => {return result.json()})
-        .then(result => {setParticipants(result)})
-        .catch(error => {return error});
+        console.log(GetAllParticipants())
+        // .then(result => {return result.json()})
+        // .then(result => setParticipants(result))
+        // .catch(error => {return error})
     }
 
     useEffect(() => {
@@ -19,7 +18,7 @@ const DTRegistration = () => {
     return (
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
                 <div className="pb-4 bg-white dark:bg-gray-900">
-                    <label for="table-search" className="sr-only">Search</label>
+                    <label htmlFor="table-search" className="sr-only">Search</label>
                     <div className="relative mt-1">
                         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                             <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
@@ -48,34 +47,7 @@ const DTRegistration = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {
-                            participants.status == 200 ?
-                                participants.body.map(items => {
-                                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                            
-                                        </th>
-                                        <td className="px-6 py-4">
-                                            Silver
-                                        </td>
-                                        <td className="px-6 py-4">
-                                            Laptop
-                                        </td>
-                                        <td className="px-6 py-4">
-                                            $2999
-                                        </td>
-                                        <td className="px-6 py-4">
-                                            <Link to=".." className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</Link>
-                                        </td>
-                                    </tr>
-                                })
-                            :
-                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    participants.messages
-                                </th>
-                            </tr>
-                        }
+                        
                     </tbody>
                 </table>
             </div>
