fbe464c41de1b5ad14276e599b20aec4d6099661
Jon <joncaesar@gmail.com>
2023-04-06 14:51:45 +0800

    Update Participants.jsx

--- src/views/Admin-dashboard/Participants/Participants.jsx
+++ src/views/Admin-dashboard/Participants/Participants.jsx
@@ -66,7 +66,7 @@ const Participants = () => {
                       <svg fill="none" stroke="currentColor" className={`w-5 h-5 mr-2 ${currentTab === 'registration' ? "text-Red" : "text-gray-500 group-hover:text-Red"}`} strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"></path>
                       </svg>
-                      <label className='font-semibold'>Register</label>
+                      <label className='font-semibold'>Registration</label>
                     </Link>
                 </li>
                 <li className="mr-2">
