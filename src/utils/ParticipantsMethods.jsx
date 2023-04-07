b44f4e228d5f0b15862caaa1520bb7753fba5582
Jon <joncaesar@gmail.com>
2023-04-07 21:05:38 +0800

    Update ParticipantsMethods.jsx

--- src/utils/ParticipantsMethods.jsx
+++ src/utils/ParticipantsMethods.jsx
@@ -54,3 +54,45 @@ export function AddParticipant (firstName, middleName, lastName, nickname, mobil
 
     return fetch(serverRoutes.participants, requestOptions)
 }
+
+
+export function UpdateParticipant (participant, firstName, middleName, lastName, nickname, mobile, email, birthday, gender, civilStatus, spouse, religion, baptized, confirmed, memberAddressLine1, memberAddressLine2, memberCity, occupation, specialty, company, companyAddressLine1, companyAddressLine2, companyCity) {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        'member_id': participant,
+        'first_name': firstName,
+        "middle_name": middleName,
+        "last_name": lastName,
+        "nickname": nickname,
+        "birthday": birthday,
+        "gender": gender,
+        "civil_status": civilStatus,
+        "spouse_member_id": spouse,
+        "religion": religion,
+        "baptism": baptized,
+        "confirmation": confirmed,
+        "member_status_id": 1,
+        "member_address_line1": memberAddressLine1,
+        "member_address_line2": memberAddressLine2,
+        "member_city": memberCity,
+        "member_mobile": mobile,
+        "email": email,
+        "occupation_name": occupation,
+        "specialty": specialty,
+        "company": company,
+        "company_address_line1": companyAddressLine1,
+        "company_address_line2": companyAddressLine2,
+        "company_city": companyCity
+    });
+
+    var requestOptions = {
+        method: "PUT",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+    };
+
+    return fetch(serverRoutes.participants, requestOptions)
+}
