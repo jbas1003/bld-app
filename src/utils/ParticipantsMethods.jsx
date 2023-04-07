81203e1a2f47e98ee4a1bdc67fc7c26a0d588fbb
Jon <joncaesar@gmail.com>
2023-04-07 12:04:27 +0800

    Update ParticipantsMethods.jsx

--- src/utils/ParticipantsMethods.jsx
+++ src/utils/ParticipantsMethods.jsx
@@ -1,13 +1,55 @@
 import { serverRoutes } from "./constants";
 
-export function GetAllParticipants (callback) {
+export function GetAllParticipants () {
     var myHeaders = new Headers();
-    myHeaders.append("Content-type", "application-json");
+    myHeaders.append("Content-type", "application/json");
 
     var requestOptions = {
         method: 'GET',
         headers: myHeaders,
         redirect: 'follow'
+    };
+
+    return fetch(serverRoutes.participants, requestOptions)
+}
+
+export function AddParticipant (firstName, middleName, lastName, nickname, mobile, email, birthday, gender, civilStatus, spouse, religion, baptized, confirmed, memberAddressLine1, memberAddressLine2, memberCity, occupation, specialty, company, companyAddressLine1, companyAddressLine2, companyCity) {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        'event_name': "Singles Encounter 15",
+        'first_name': firstName,
+        "middle_name": middleName,
+        "last_name": lastName,
+        "nickname": nickname,
+        "birthday": birthday,
+        "gender": gender,
+        "civil_status": civilStatus,
+        "spouse_member_id": spouse,
+        "religion": religion,
+        "baptism": baptized,
+        "confirmation": confirmed,
+        "member_status_id": 1,
+        "created_by": 1,
+        "member_address_line1": memberAddressLine1,
+        "member_address_line2": memberAddressLine2,
+        "member_city": memberCity,
+        "member_mobile": mobile,
+        "email": email,
+        "occupation_name": occupation,
+        "specialty": specialty,
+        "company": company,
+        "company_address_line1": companyAddressLine1,
+        "company_address_line2": companyAddressLine2,
+        "company_city": companyCity
+    });
+
+    var requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
     };
 
     return fetch(serverRoutes.participants, requestOptions)
