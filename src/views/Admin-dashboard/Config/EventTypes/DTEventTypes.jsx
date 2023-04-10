0d04a56bda978d933c094f63ad445ce4c612d956
Jon <joncaesar@gmail.com>
2023-04-10 20:38:06 +0800

    Update DTEventTypes.jsx

--- src/views/Admin-dashboard/Config/EventTypes/DTEventTypes.jsx
+++ src/views/Admin-dashboard/Config/EventTypes/DTEventTypes.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { AddNewEventTypes, DeleteEventType, GetAllEventTypes } from '../../../../utils/EventTypeMethods';
+import { AddNewEventTypes, DeleteEventType, GetAllEventTypes, UpdateEventType } from '../../../../utils/EventTypeMethods';
 import AddEventTypes from './Modals/AddEventTypes';
 import EditEventTypes from './Modals/EditEventTypes';
 import DeleteEventTypeWarning from './Modals/DeleteEventTypeWarning';
@@ -15,6 +14,7 @@ const DTEventTypes = () => {
     const [showEdit, setShowEdit] = useState();
     const [showDeleteWarning, setShowDeleteWarning] = useState();
 
+    const [eventTypeId, setEventTypeId] = useState();
     const [eventTypeName, setEventTypeName] = useState();
     const [eventTypeCategory, setEventTypeCategory] = useState();
 
@@ -28,7 +28,7 @@ const DTEventTypes = () => {
                     setEventType(result.body)
                 } else {
                     setStatus(false)
-                    setMessage(result.errors)
+                    setMessage(result.message)
                 }
             })
         setIsLoading(false);
@@ -37,30 +37,70 @@ const DTEventTypes = () => {
     const addNewEventType = () => {
         setIsLoading(true);
         AddNewEventTypes(eventTypeName, eventTypeCategory)
-            .then(result => {return result.json()})
-            .then(result => {
-                if (result.status === 200) {
-                    setMessage(result.message)
+            .then(async result => {return await result.json()})
+            .then(async result => {
+                if (await result.status === 200) {
+                    alert(`${eventTypeName} was added successfully!`)
+                } else if (await result.status === 422) {
+                    alert(`${eventTypeName} already exist.`)
                 } else {
-                    setMessage(result.message)
+                    alert(`A problem occured while adding ${eventTypeName} to the records. Please contact your systems administrator.`)
                 }
             })
         setIsLoading(false);
-        
-        if (isLoading) {
-            
-        } else {
-            alert(message)
-        }
+
+        setEventTypeName('');
+        setEventTypeCategory('');
+        setShowAdd(false);
+        setMessage('');
+        getEventTypes();
     }
 
-    const editEventType = (eventyTypeName, eventTypeCategory) => {
-        setEventTypeName(eventyTypeName);
+    const editEventType = (eventTypeId, eventTypeName, eventTypeCategory) => {
+        setEventTypeId(eventTypeId);
+        setEventTypeName(eventTypeName);
         setEventTypeCategory(eventTypeCategory);
         setShowEdit(true);
     }
 
-    const showDelete = (eventTypeName) => {
+    const updateEventType = () => {
+        setIsLoading(true);
+        UpdateEventType(eventTypeId, eventTypeName, eventTypeCategory)
+            .then(async result => {return await result.json()})
+            .then(async result => {
+                if (await result.status === 200) {
+                    alert('Record was successfull updated!');
+                } else if(await result.status === 422) {
+                    alert(`No changes were made.`)
+                } else {
+                    alert('An error occured while trying to update the records. Please contact system administrators.')
+                }
+            });
+        
+            closeEdit();
+            getEventTypes();
+    }
+
+    const deleteEventType = () => {
+        setIsLoading(true);
+        DeleteEventType(eventTypeId)
+            .then(async result => {return await result.json()})
+            .then(async result => {
+                if (await result.status === 200) {
+                    setStatus(true);
+                    alert(`${eventTypeName} was deleted successfully.`)
+                } else {
+                    setStatus(false);
+                    alert('Sever error: A problem was encountered while deleting the record.')
+                }
+            })
+        setIsLoading(false);
+        closeDelete();
+        getEventTypes();
+    }
+
+    const showDelete = (eventTypeId, eventTypeName) => {
+        setEventTypeId(eventTypeId);
         setEventTypeName(eventTypeName);
         setShowDeleteWarning(true);
     }
@@ -72,8 +112,9 @@ const DTEventTypes = () => {
     }
 
     const closeEdit = () => {
+        setEventTypeId('');
         setEventTypeName('');
-        setEventTypeCategory('');
+        setEventTypeCategory('')
         setShowEdit(false);
     }
 
@@ -170,7 +211,7 @@ const DTEventTypes = () => {
                                         <td className="px-6 py-4 whitespace-nowrap" style={{ cursor: "pointer", width: "20%" }}>
                                             <button type="button"
                                                 className="text-red-800 border border-red-800 hover:bg-red-800 hover:text-white focus:ring-4 focus:outline-none focus:ring-white font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:focus:ring-red-800"
-                                                onClick={() => showDelete(items.event_type_name)}
+                                                onClick={() => showDelete(items.event_type_id, items.event_type_name)}
                                             >
                                                 <svg className="w-3 h-3" fill="none" stroke="currentColor" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                                     <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
@@ -179,7 +220,7 @@ const DTEventTypes = () => {
 
                                             <button type="button"
                                                 className="text-green-800 border border-green-800 hover:bg-green-800 hover:text-white focus:ring-4 focus:outline-none focus:ring-white font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:focus:ring-green-800"
-                                                onClick={() => editEventType(items.event_type_name, items.event_type_category)}
+                                                onClick={() => editEventType(items.event_type_id, items.event_type_name, items.event_type_category)}
                                             >
                                                 <svg className="w-3 h-3" fill="none" stroke="currentColor" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                                     <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
@@ -287,7 +328,7 @@ const DTEventTypes = () => {
                     </div>
                 </div>
                 <button type="submit" className="mx-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                    // onClick={() => Stepper('work')}
+                    onClick={updateEventType}
                 >
                     Save
                 </button>
@@ -305,7 +346,7 @@ const DTEventTypes = () => {
                 <div className="flex items-center justify-around gap-4 mb-4 sm:grid-cols-2 rounded-lg">
                     <div className='flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg'>
                         <button type="submit" className="mx-3 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                            // onClick={() => Stepper('work')}
+                            onClick={deleteEventType}
                         >
                             Yes
                         </button>
