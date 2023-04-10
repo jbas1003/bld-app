6de1998d66883d71ea3a100c9682f75ed9f51bb7
Jon <joncaesar@gmail.com>
2023-04-10 20:37:30 +0800

    Update EventTypeMethods.jsx

--- src/utils/EventTypeMethods.jsx
+++ src/utils/EventTypeMethods.jsx
@@ -34,6 +34,25 @@ export function AddNewEventTypes (eventTypeName, eventTypeCategory) {
     return fetch(serverRoutes.eventTypes, requestOptions);
 }
 
+export function UpdateEventType (eventTypeId, eventTypeName) {
+    var myHeaders = new Headers();
+    myHeaders.append('Content-type', 'application/json');
+
+    var raw = JSON.stringify({
+        'event_type_id': eventTypeId,
+        'event_type_name': eventTypeName
+    });
+
+    var requestOptions = {
+        method: 'PUT',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    return fetch(serverRoutes.eventTypes, requestOptions);
+}
+
 
 export function DeleteEventType (eventTypeId) {
     var myHeaders = new Headers();
@@ -47,7 +66,7 @@ export function DeleteEventType (eventTypeId) {
         method: 'DELETE',
         headers: myHeaders,
         body: raw,
-        redirec: 'follow'
+        redirect: 'follow'
     };
 
     return fetch(serverRoutes.eventTypes, requestOptions);
