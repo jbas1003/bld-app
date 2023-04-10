b2c43e146fa51ad8e5830823a7935cdf1386b2e2
Jon <joncaesar@gmail.com>
2023-04-10 20:37:32 +0800

    Update Config.jsx

--- src/views/Admin-dashboard/Config/Config.jsx
+++ src/views/Admin-dashboard/Config/Config.jsx
@@ -1,92 +1,109 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import EventTypes from './EventTypes/EventTypes';
 import DTEventTypes from './EventTypes/DTEventTypes';
+import EventTypeCategories from './EventTypeCategories/EventTypeCategories';
+import DTEventTypeCategories from './EventTypeCategories/DTEventTypeCategories';
+import Events from './Events/Events';
+import DTEvents from './Events/DTEvents';
 
 const Config = () => {
     const [currentTab, setCurrentTab] = useState();
     const [showEventTypes, setShowEventTypes] = useState();
-    const [showRooms, setShowRooms] = useState();
-    const [showTribes, setShowTribes] = useState();
-    const [showNations, setShowNations] = useState();
+    const [showEvents, setShowEvents] = useState();
+    const [showMemberStatus, setShowMemberStatus] = useState();
+    const [showEventTypeCategories, setShowEventTypeCategories] = useState();
 
     const CurrentTab = (tab) => {
         setCurrentTab(tab);
-
         switch (tab) {
+
+            case "eventTypeCategories":
+              setShowEventTypeCategories(true);
+              setShowEventTypes(false);
+              setShowEvents(false);
+              setShowMemberStatus(false);
+              break;
+
             case "eventTypes":
+                setShowEventTypeCategories(false);
                 setShowEventTypes(true);
-                setShowRooms(false);
-                setShowTribes(false);
-                setShowNations(false);
+                setShowEvents(false);
+                setShowMemberStatus(false);
+                setShowEventTypeCategories(false);
                 break;
             
-            case "rooms":
+            case "events":
+                setShowEventTypeCategories(false);
                 setShowEventTypes(false);
-                setShowRooms(true);
-                setShowTribes(false);
-                setShowNations(false);
+                setShowEvents(true);
+                setShowMemberStatus(false);
+                setShowEventTypeCategories(false);
                 break;
 
-            case "tribes":
+            case "memberStatus":
+                setShowEventTypeCategories(false);
                 setShowEventTypes(false);
-            setShowRooms(false);
-            setShowTribes(true);
-            setShowNations(false);
-            break;
-
-            case "nations":
-                setShowEventTypes(false);
-                setShowRooms(false);
-                setShowTribes(false);
-                setShowNations(true);
+                setShowEvents(false);
+                setShowMemberStatus(true);
+                setShowEventTypeCategories(false);
                 break;
 
             default:
                 break;
         }
     }
+
+    
     return (
         
         <div className="font-alegreya border-b border-gray-100 mb-10">
             <ul className="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500">
                 <li className="mr-2">
+                    <Link onClick={() => CurrentTab("eventTypeCategories")} className={`inline-flex p-4 border-b-2 ${currentTab === 'eventTypeCategories' ? "text-Red border-Red rounded-t-lg active" : "border-transparent rounded-t-lg hover:text-Red hover:border-Red"} group`}>
+                      <svg fill="none" stroke="currentColor" className={`w-5 h-5 mr-2 ${currentTab === 'eventTypeCategories' ? "text-Red" : "text-gray-500 group-hover:text-Red"}`} strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 01-1.125-1.125v-3.75zM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-8.25zM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-2.25z" />
+                      </svg>
+                      <label className='font-semibold'>Event Type Categories</label>
+                    </Link>
+                </li>
+                <li className="mr-2">
                     <Link onClick={() => CurrentTab("eventTypes")} className={`inline-flex p-4 border-b-2 ${currentTab === 'eventTypes' ? "text-Red border-Red rounded-t-lg active" : "border-transparent rounded-t-lg hover:text-Red hover:border-Red"} group`}>
                       <svg fill="none" stroke="currentColor" className={`w-5 h-5 mr-2 ${currentTab === 'eventTypes' ? "text-Red" : "text-gray-500 group-hover:text-Red"}`} strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"></path>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 7.5l3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0021 18V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v12a2.25 2.25 0 002.25 2.25z" />
                       </svg>
                       <label className='font-semibold'>Event Types</label>
                     </Link>
                 </li>
                 <li className="mr-2">
-                    <Link onClick={() => CurrentTab("rooms")} className={`inline-flex p-4 border-b-2 ${currentTab === 'rooms' ? "text-Red border-Red rounded-t-lg active" : "border-transparent rounded-t-lg hover:text-Red hover:border-Red"} group`}>
-                      <svg fill="none" stroke="currentColor" className={`w-5 h-5 mr-2 ${currentTab === 'rooms' ? "text-Red" : "text-gray-500 group-hover:text-Red"}`} strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"></path>
+                    <Link onClick={() => CurrentTab("events")} className={`inline-flex p-4 border-b-2 ${currentTab === 'events' ? "text-Red border-Red rounded-t-lg active" : "border-transparent rounded-t-lg hover:text-Red hover:border-Red"} group`}>
+                      <svg fill="none" stroke="currentColor" className={`w-5 h-5 mr-2 ${currentTab === 'events' ? "text-Red" : "text-gray-500 group-hover:text-Red"}`} strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 010 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 010-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375z" />
                       </svg>
-                      <label className='font-semibold'>Rooms</label>
+                      <label className='font-semibold'>Events</label>
                     </Link>
                 </li>
                 <li className="mr-2">
-                    <Link onClick={() => CurrentTab("tribes")} className={`inline-flex p-4 border-b-2 ${currentTab === 'tribes' ? "text-Red border-Red rounded-t-lg active" : "border-transparent rounded-t-lg hover:text-Red hover:border-Red"} group`}>
-                      <svg fill="none" stroke="currentColor" className={`w-5 h-5 mr-2 ${currentTab === 'tribes' ? "text-Red" : "text-gray-500 group-hover:text-Red"}`} strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"></path>
+                    <Link onClick={() => CurrentTab("memberStatus")} className={`inline-flex p-4 border-b-2 ${currentTab === 'memberStatus' ? "text-Red border-Red rounded-t-lg active" : "border-transparent rounded-t-lg hover:text-Red hover:border-Red"} group`}>
+                      <svg fill="none" stroke="currentColor" className={`w-5 h-5 mr-2 ${currentTab === 'memberStatus' ? "text-Red" : "text-gray-500 group-hover:text-Red"}`} strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
                       </svg>
-                      <label className='font-semibold'>Tribes</label>
-                    </Link>
-                </li>
-                <li className="mr-2">
-                    <Link onClick={() => CurrentTab("nations")} className={`inline-flex p-4 border-b-2 ${currentTab === 'nations' ? "text-Red border-Red rounded-t-lg active" : "border-transparent rounded-t-lg hover:text-Red hover:border-Red"} group`}>
-                      <svg fill="none" stroke="currentColor" className={`w-5 h-5 mr-2 ${currentTab === 'nations' ? "text-Red" : "text-gray-500 group-hover:text-Red"}`} strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 3v1.5M3 21v-6m0 0l2.77-.693a9 9 0 016.208.682l.108.054a9 9 0 006.086.71l3.114-.732a48.524 48.524 0 01-.005-10.499l-3.11.732a9 9 0 01-6.085-.711l-.108-.054a9 9 0 00-6.208-.682L3 4.5M3 15V4.5"></path>
-                      </svg>
-                      <label className='font-semibold'>Nations</label>
+                      <label className='font-semibold'>Member Statuses</label>
                     </Link>
                 </li>
             </ul>
 
+            <EventTypeCategories show={showEventTypeCategories}>
+              <DTEventTypeCategories />
+            </EventTypeCategories>
+
             <EventTypes show={showEventTypes}>
                 <DTEventTypes />
             </EventTypes>
+
+            <Events show={showEvents}>
+              <DTEvents />
+            </Events>
         </div>
 
     )
