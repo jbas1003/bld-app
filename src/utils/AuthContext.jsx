a9a0b9742438a0f943e3c1e395a4fbb085529480
Jon <joncaesar@gmail.com>
2023-04-14 14:26:20 +0800

    Update AuthContext.jsx

--- src/utils/AuthContext.jsx
+++ src/utils/AuthContext.jsx
@@ -7,31 +7,31 @@ export const AuthProvider = ({ children }) => {
     const [loginResult, setLoginResult] = useState();
     const [loginMessage, setLoginMessage] = useState();
 
-    function login (username, password) {
-
-        Login(username, password)
-            .then(async result => {return await result.json()})
-            .then(async result => {
-                if (await result.status === 200) {
-                    const loginData = {
-                        "first_name": result.body.first_name,
-                        "middle_name": result.body.middle_name,
-                        "last_name": result.body.middle_name
-                    };
+    const memberLogin = (username, password) => {
+        console.log(`${username} and ${password}`);
+        // Login(username, password)
+        //     .then(async result => {return await result.json()})
+        //     .then(async result => {
+        //         if (await result.status === 200) {
+        //             const loginData = {
+        //                 "first_name": result.body.first_name,
+        //                 "middle_name": result.body.middle_name,
+        //                 "last_name": result.body.middle_name
+        //             };
                     
-                    window.sessionStorage.setItem('___', JSON.stringify(loginData));
-                    setLoginResult(JSON.parse(window.sessionStorage.getItem("___")));
-                } else {
-                    setLoginMessage(result.message);
-                }
-            });
+        //             window.sessionStorage.setItem('___', JSON.stringify(loginData));
+        //             setLoginResult(JSON.parse(window.sessionStorage.getItem("___")));
+        //         } else {
+        //             setLoginMessage(result.message);
+        //         }
+        //     });
     }
 
     useEffect(() => {
         setLoginResult(JSON.parse(window.sessionStorage.getItem("___")));
     }, [])
 
-    return <AuthContext.Provider value={{ loginResult, login, loginMessage }}>
+    return <AuthContext.Provider value={{ memberLogin, loginResult, loginMessage }}>
         { children }
     </AuthContext.Provider>
 }
