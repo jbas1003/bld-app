4bb14974542f417a703e54c8508574eca62e5c32
Jon <joncaesar@gmail.com>
2023-04-15 23:48:08 +0800

    Create AttendanceMethod.jsx

---
+++ src/utils/AttendanceMethod.jsx
@@ -0,0 +1,40 @@
+import { serverRoutes } from "./constants";
+
+export function GetAttendance(id) {
+    var myHeader = new Headers();
+    myHeader.append('Content-Type', 'application/json');
+
+    var raw = JSON.stringify({
+        'event_id': id
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeader,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    return fetch(serverRoutes.showAttendances, requestOptions);
+}
+
+export function CreateAttendanceRecord (id, memberId, eventId, status) {
+    var myHeader = new Headers();
+    myHeader.append('Content-Type', 'application/json');
+
+    var raw = JSON.stringify({
+        'member_id': memberId,
+        'event_id': eventId,
+        'status': status,
+        'created_by': id
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeader,
+        body: raw,
+        redirect: 'follow'
+    };
+    console.log(raw);
+    // return fetch(serverRoutes.attendances, requestOptions);
+}
