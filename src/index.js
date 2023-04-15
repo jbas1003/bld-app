92cfc15476497def9604712ff128c6706f46568f
Jon <joncaesar@gmail.com>
2023-04-15 11:09:04 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from './utils/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
    </BrowserRouter>
   </React.StrictMode>
 );
