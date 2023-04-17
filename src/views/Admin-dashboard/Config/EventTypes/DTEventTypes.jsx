36cc5692c876d5b323395850421cfe37a9e13d5b
Jon <joncaesar@gmail.com>
2023-04-18 00:03:16 +0800

    Update DTEventTypes.jsx

--- src/views/Admin-dashboard/Config/EventTypes/DTEventTypes.jsx
+++ src/views/Admin-dashboard/Config/EventTypes/DTEventTypes.jsx
@@ -269,7 +269,7 @@ const DTEventTypes = () => {
                                         :""
                                     }
                             >
-                                <option selected value={" "}>Choose Event Type Category</option>
+                                <option defaultValue={" "}>Choose Event Type Category</option>
                                 {
                                     status ?
                                         eventType.map(items => (
@@ -283,11 +283,13 @@ const DTEventTypes = () => {
                         </div>
                     </div>
                 </div>
-                <button type="submit" className="mx-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                    onClick={addNewEventType}
-                >
-                    Save
-                </button>
+                <div className='flex justify-end w-full'>
+                    <button type="submit" className="mx-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        onClick={addNewEventType}
+                    >
+                        Save
+                    </button>
+                </div>
             </AddEventTypes>
 
             <EditEventTypes show={showEdit} setShow={closeEdit}>
@@ -315,7 +317,7 @@ const DTEventTypes = () => {
                                         :""
                                     }
                             >
-                                <option selected value={" "}>Choose Event Type Category</option>
+                                <option defaultValue={" "}>Choose Event Type Category</option>
                                 {
                                     status ?
                                         eventType.map(items => (
