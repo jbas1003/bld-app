21026123df925dd3bd06644e1c2432b42a1ea52e
Jon <joncaesar@gmail.com>
2023-04-19 07:18:29 +0800

    Update Participants.jsx

--- src/views/Admin-dashboard/Participants/Participants.jsx
+++ src/views/Admin-dashboard/Participants/Participants.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import DTRegistration from './Registration/DTRegistration';
 import Registration from './Registration/Registration';
-import Tribes from './Tribes/Tribes';
-import DTTribes from './Tribes/DTTribes';
 import Nations from './Nations/Nations';
 import DTNations from './Nations/DTNations';
 import CPW from './CPW/CPW';
 import DTCPW from './CPW/DTCPW';
+import WSC from './WSC/WSC';
+import DTWSC from './WSC/DTWSC';
 
 const Participants = () => {
 
@@ -36,7 +36,7 @@ const Participants = () => {
             setShowNations(false);
             break;
 
-        case "tribes":
+        case "wsc":
           setShowRegistration(false);
           setShowCPW(false);
           setShowTribes(true);
@@ -78,11 +78,11 @@ const Participants = () => {
                     </Link>
                 </li>
                 <li className="mr-2">
-                    <Link onClick={() => CurrentTab("tribes")} className={`inline-flex p-4 border-b-2 ${currentTab === 'tribes' ? "text-Red border-Red rounded-t-lg active" : "border-transparent rounded-t-lg hover:text-Red hover:border-Red"} group`}>
-                      <svg fill="none" stroke="currentColor" className={`w-5 h-5 mr-2 ${currentTab === 'tribes' ? "text-Red" : "text-gray-500 group-hover:text-Red"}`} strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"></path>
+                    <Link onClick={() => CurrentTab("wsc")} className={`inline-flex p-4 border-b-2 ${currentTab === 'wsc' ? "text-Red border-Red rounded-t-lg active" : "border-transparent rounded-t-lg hover:text-Red hover:border-Red"} group`}>
+                      <svg fill="none" stroke="currentColor" className={`w-5 h-5 mr-2 ${currentTab === 'wsc' ? "text-Red" : "text-gray-500 group-hover:text-Red"}`} strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
                       </svg>
-                      <label className='font-semibold'>Tribes</label>
+                      <label className='font-semibold'>WSC Attendance</label>
                     </Link>
                 </li>
                 <li className="mr-2">
@@ -104,9 +104,9 @@ const Participants = () => {
           <DTCPW />
         </CPW>
 
-        <Tribes show={showTribes}>
-          <DTTribes />
-        </Tribes>
+        <WSC show={showTribes}>
+          <DTWSC />
+        </WSC>
 
         <Nations show={showNations}>
           <DTNations />
