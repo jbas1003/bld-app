4793ae265f2baa569d2e181784ec854746657bd5
Jon <joncaesar@gmail.com>
2023-04-18 21:27:02 +0800

    Update DTCPW.jsx

--- src/views/Admin-dashboard/Participants/CPW/DTCPW.jsx
+++ src/views/Admin-dashboard/Participants/CPW/DTCPW.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { GetAllEvents } from '../../../../utils/EventsMethods';
 import { GetAttendanceList } from '../../../../utils/AttendanceMethod';
 
 const DTCPW = () => {
