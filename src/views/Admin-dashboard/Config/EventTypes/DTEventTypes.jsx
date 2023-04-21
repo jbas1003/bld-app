fa98bd025907887ddad0aadfcd3a347c63b9a1d9
Jon <joncaesar@gmail.com>
2023-04-22 05:31:41 +0800

    Update DTEventTypes.jsx

--- src/views/Admin-dashboard/Config/EventTypes/DTEventTypes.jsx
+++ src/views/Admin-dashboard/Config/EventTypes/DTEventTypes.jsx
@@ -174,75 +174,77 @@ const DTEventTypes = () => {
                     </button>
                 </div>
             </div>
-            <table id='eventTypeDTbl' className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                        <th scope="col" className="px-6 py-3">
-                            Event Type Name
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Event Type Category
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Actions
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        
-                        status ?
-                            eventType.map(items => (
-                                
-                                items.event_type_category !== null ?
-                                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                            <p className='hover:cursor-pointer'>{items.event_type_name}</p>
-                                        </th>
-                                        <td className="px-6 py-4">
-                                            <p className='hover:cursor-pointer'>
-                                            {
-                                                eventType.map(category => (
-                                                    category.event_type_id === items.event_type_category ?
-                                                        category.event_type_name
-                                                    :null
-                                                ))
-                                            }
-                                            </p>
-                                        </td>
-                                        <td className="px-6 py-4 whitespace-nowrap" style={{ cursor: "pointer", width: "20%" }}>
-                                            <button type="button"
-                                                className="text-red-800 border border-red-800 hover:bg-red-800 hover:text-white focus:ring-4 focus:outline-none focus:ring-white font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:focus:ring-red-800"
-                                                onClick={() => showDelete(items.event_type_id, items.event_type_name)}
-                                            >
-                                                <svg className="w-3 h-3" fill="none" stroke="currentColor" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                                                </svg>
-                                            </button>
+            <div className='overflow-x-auto overflow-scroll max-h-[600px]'>
+                <table id='eventTypeDTbl' className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" className="px-6 py-3">
+                                Event Type Name
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Event Type Category
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Actions
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            
+                            status ?
+                                eventType.map(items => (
+                                    
+                                    items.event_type_category !== null ?
+                                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                <p className='hover:cursor-pointer'>{items.event_type_name}</p>
+                                            </th>
+                                            <td className="px-6 py-4">
+                                                <p className='hover:cursor-pointer'>
+                                                {
+                                                    eventType.map(category => (
+                                                        category.event_type_id === items.event_type_category ?
+                                                            category.event_type_name
+                                                        :null
+                                                    ))
+                                                }
+                                                </p>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-nowrap" style={{ cursor: "pointer", width: "20%" }}>
+                                                <button type="button"
+                                                    className="text-red-800 border border-red-800 hover:bg-red-800 hover:text-white focus:ring-4 focus:outline-none focus:ring-white font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:focus:ring-red-800"
+                                                    onClick={() => showDelete(items.event_type_id, items.event_type_name)}
+                                                >
+                                                    <svg className="w-3 h-3" fill="none" stroke="currentColor" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                                        <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                                    </svg>
+                                                </button>
 
-                                            <button type="button"
-                                                className="text-green-800 border border-green-800 hover:bg-green-800 hover:text-white focus:ring-4 focus:outline-none focus:ring-white font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:focus:ring-green-800"
-                                                onClick={() => editEventType(items.event_type_id, items.event_type_name, items.event_type_category)}
-                                            >
-                                                <svg className="w-3 h-3" fill="none" stroke="currentColor" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                                                </svg>
-                                            </button>
+                                                <button type="button"
+                                                    className="text-green-800 border border-green-800 hover:bg-green-800 hover:text-white focus:ring-4 focus:outline-none focus:ring-white font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:focus:ring-green-800"
+                                                    onClick={() => editEventType(items.event_type_id, items.event_type_name, items.event_type_category)}
+                                                >
+                                                    <svg className="w-3 h-3" fill="none" stroke="currentColor" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                                    </svg>
+                                                </button>
+                                        </td>
+                                        </tr>
+                                    :null
+                                
+                                ))
+                            :
+                                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                    <td colSpan={3}  className="px-6 py-4 font-medium text-center text-gray-900 whitespace-nowrap dark:text-white">
+                                        {message}
                                     </td>
-                                    </tr>
-                                :null
-                            
-                            ))
-                        :
-                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <td colSpan={3}  className="px-6 py-4 font-medium text-center text-gray-900 whitespace-nowrap dark:text-white">
-                                    {message}
-                                </td>
-                            </tr>
-                            
-                    }
-                </tbody>
-            </table>
+                                </tr>
+                                
+                        }
+                    </tbody>
+                </table>
+            </div>
 
             <AddEventTypes show={showAdd} setShow={closeAdd}>
                 <div className="grid gap-4 mb-4 sm:grid-cols-2">
