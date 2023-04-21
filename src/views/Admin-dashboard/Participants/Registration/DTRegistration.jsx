0a66841dec8f051bb45e357a9b152b7a8051897f
Jon <joncaesar@gmail.com>
2023-04-22 05:31:51 +0800

    Update DTRegistration.jsx

--- src/views/Admin-dashboard/Participants/Registration/DTRegistration.jsx
+++ src/views/Admin-dashboard/Participants/Registration/DTRegistration.jsx
@@ -427,7 +427,7 @@ const DTRegistration = () => {
                     </button>
                 </div>
             </div>
-            <div className='overflow-x-auto overflow-scroll h-[600px]'>
+            <div className='overflow-x-auto overflow-scroll max-h-[600px]'>
                 <table id='participantDTbl' className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
