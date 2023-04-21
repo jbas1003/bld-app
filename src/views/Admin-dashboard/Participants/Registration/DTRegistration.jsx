4b01295329bb194cf65f46f203efb67fa96889d3
Jon <joncaesar@gmail.com>
2023-04-21 19:35:43 +0800

    Update DTRegistration.jsx

--- src/views/Admin-dashboard/Participants/Registration/DTRegistration.jsx
+++ src/views/Admin-dashboard/Participants/Registration/DTRegistration.jsx
@@ -529,14 +529,14 @@ const DTRegistration = () => {
                                                     </button>
                                                 : null
                                             }
-                                            <button type="button"
+                                            {/* <button type="button"
                                                 className="text-red-800 border border-red-800 hover:bg-red-800 hover:text-white focus:ring-4 focus:outline-none focus:ring-white font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:focus:ring-red-800"
                                                 // onClick={() => ShowDeleteWarning(items.id, items.first_name, items.last_name)}
                                             >
                                                 <svg className="w-3 h-3" fill="none" stroke="currentColor" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                                     <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                                 </svg>
-                                            </button>
+                                            </button> */}
                                         </td>
                                     </tr>
                                 ))
