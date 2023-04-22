7dcf230cb730c67d0e9176c5e2c1e44ad52c1772
Jon <joncaesar@gmail.com>
2023-04-22 17:51:07 +0800

    Update Participants.jsx

--- src/views/Admin-dashboard/Participants/Participants.jsx
+++ src/views/Admin-dashboard/Participants/Participants.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
 import DTRegistration from './Registration/DTRegistration';
 import Registration from './Registration/Registration';
 import CPW from './CPW/CPW';
