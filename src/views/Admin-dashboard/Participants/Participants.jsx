b95ef7ddb08c42d59ae353956fa380e01be56c27
Jon <joncaesar@gmail.com>
2023-04-23 05:55:23 +0800

    Update Participants.jsx

--- src/views/Admin-dashboard/Participants/Participants.jsx
+++ src/views/Admin-dashboard/Participants/Participants.jsx
@@ -73,7 +73,7 @@ const Participants = () => {
                       <svg fill="none" stroke="currentColor" className={`w-5 h-5 mr-2 ${currentTab === 'cpw' ? "text-Red" : "text-gray-500 group-hover:text-Red"}`} strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z" />
                       </svg>
-                      <label className='font-semibold'>CPW Attendance</label>
+                      <label className='font-semibold'>CPW Attendance Record</label>
                     </Link>
                 </li>
                 <li className="mr-2">
@@ -81,7 +81,7 @@ const Participants = () => {
                       <svg fill="none" stroke="currentColor" className={`w-5 h-5 mr-2 ${currentTab === 'wsc' ? "text-Red" : "text-gray-500 group-hover:text-Red"}`} strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
                       </svg>
-                      <label className='font-semibold'>WSC Attendance</label>
+                      <label className='font-semibold'>WSC Attendance Record</label>
                     </Link>
                 </li>
                 <li className="mr-2">
@@ -89,7 +89,7 @@ const Participants = () => {
                       <svg fill="none" stroke="currentColor" className={`w-5 h-5 mr-2 ${currentTab === 'teachings' ? "text-Red" : "text-gray-500 group-hover:text-Red"}`} strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />
                       </svg>
-                      <label className='font-semibold'>Teachings Attendance List</label>
+                      <label className='font-semibold'>Teachings Attendance Record</label>
                     </Link>
                 </li>
             </ul>
