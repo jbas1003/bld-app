458dd6fab17dd01ccac1cdd2e8683526f454c1c2
Jon <joncaesar@gmail.com>
2023-04-25 19:42:13 +0800

    Update DTRegistration.jsx

--- src/views/Admin-dashboard/SinglesEncounter/Registration/DTRegistration.jsx
+++ src/views/Admin-dashboard/SinglesEncounter/Registration/DTRegistration.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 
 function DTRegistration() {
+
+    const [contactList, setContactList] = useState([{name: '', mobile: '', email: ''}]);
 
     function tableSearch() {
         // Declare variables
@@ -25,6 +26,23 @@ function DTRegistration() {
         }
     }
 
+    const handleContactChange = (e, index) => {
+        const {name, value} = e.target;
+        const contacts = [...contactList];
+        contacts[index][name] = value;
+        setContactList(contacts);
+    }
+
+    const handleadd = () => {
+        setContactList([...contactList, { name: '', mobile: '', email: ''}]);
+    }
+
+    const handleremove = index => {
+        const contacts = [...contactList];
+        contacts.splice(index, 1);
+        setContactList(contacts);
+    }
+
     return (
         
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
@@ -39,7 +57,7 @@ function DTRegistration() {
                     />
                 </div>
             </div>
-            <div className='overflow-x-auto overflow-scroll max-h-[600px]'>
+            {/* <div className='overflow-x-auto overflow-scroll max-h-[600px]'>
                 <table id='SEDTRegistration' className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
@@ -80,6 +98,57 @@ function DTRegistration() {
                         </tr>
                     </tbody>
                 </table>
+            </div> */}
+
+            <div className="grid gap-4 mb-4 sm:grid-cols-4">
+                {
+                    contactList.map((x, i) => (
+                        <>
+                            <div className="relative">
+                                <input type="text" id="fo_name" className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "
+                                    onChange={ e => handleContactChange(e,i) }
+                                />
+                                <label for="fo_name" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Name</label>
+                            </div>
+                            <div className="relative">
+                                <input type="text" id="fo_mobile" className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "
+                                    onChange={ e => handleContactChange(e,i) }
+                                />
+                                <label for="fo_mobile" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Mobile</label>
+                            </div>
+                            <div className="relative">
+                                <input type="text" id="fo_email" className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "
+                                    onChange={ e => handleContactChange(e,i) }
+                                />
+                                <label for="fo_email" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Email</label>
+                            </div>
+                            <div className="relative">
+                                {
+                                    contactList.length-1 !== i &&
+                                        <button type="button" className="mx-1 text-red-700 border border-red-700 hover:bg-red-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center"
+                                        onClick={ () => handleremove(i) }
+                                        >
+                                            <svg fill="none" className="w-4 h-4" stroke="currentColor" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                            </svg>
+                                            <span className="sr-only">Remove Contact</span>
+                                        </button>
+                                }
+                                {
+                                    contactList.length-1 === i &&
+                                        <button type="button" className="mx-1 text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center"
+                                        onClick={ handleadd }
+                                        >
+                                            <svg fill="none" className="w-4 h-4" stroke="currentColor" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                                            </svg>
+                                            <span className="sr-only">Add Contact</span>
+                                        </button>
+                                }
+                            </div>
+                        </>
+                    ))
+                }
             </div>
         </div>
     )
