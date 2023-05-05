0e25f3a6938d19f5bbc911cb573cf407743142de
Jon <joncaesar@gmail.com>
2023-05-05 17:15:35 +0800

    Update DTRegistration.jsx

--- src/views/Admin-dashboard/Attendance/Registration/DTRegistration.jsx
+++ src/views/Admin-dashboard/Attendance/Registration/DTRegistration.jsx
@@ -399,7 +399,7 @@ const DTRegistration = () => {
                         : alert('No Event was selected. Please select an event.')
                         }
                     >
-                        <option defaultValue={" "}>Choose an event</option>
+                        <option value={null}>Choose an event</option>
                         {
                             status ?
                                 eventData !== null & eventData !== undefined ?
@@ -515,7 +515,7 @@ const DTRegistration = () => {
                                                     <svg className="w-3 h-3" fill="none" stroke="currentColor" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                                         <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                                                     </svg>
-                                                </button>
+                                            </button>
                                             {
                                                 items.status !== '' & items.status !== null & items.status !== undefined ?
                                                     <button type="button"
