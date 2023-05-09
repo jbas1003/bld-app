b2c801695dd3861726c31451eea82e095445cff3
Jon <joncaesar@gmail.com>
2023-05-09 12:25:25 +0800

    Update SinglesEncounterMethods.jsx

--- src/utils/SinglesEncounterMethods.jsx
+++ src/utils/SinglesEncounterMethods.jsx
@@ -65,3 +65,51 @@ export function AddParticipant (id, firstName, middleName, lastName,
         
         return fetch(serverRoutes.se, requestOptions);
 }
+
+export function UpdateParticipant (userId, memberId, seId, firstName, middleName,
+                                    lastName, nickname, memberMobile, memberEmail,
+                                    birthday, gender, civil_status, religion, baptism,
+                                    confirmation, member_addressLine1, member_addressLine2,
+                                    member_city, occupation, specialty, company,
+                                    work_addressLine1, work_addressLine2, work_city,
+                                    emergency_contacts) {
+    
+    const myHeader = new Headers();
+    myHeader.append('Content-Type', 'application/json');
+
+    const raw = JSON.stringify({
+        member_id: memberId,
+        created_by: userId,
+        first_name: firstName,
+        middle_name: middleName,
+        last_name: lastName,
+        nickname: nickname,
+        birthday: birthday,
+        gender: gender,
+        civil_status: civil_status,
+        religion: religion,
+        baptism: baptism,
+        confirmation: confirmation,
+        member_addressLine1: member_addressLine1,
+        member_addressLine2: member_addressLine2,
+        member_city: member_city,
+        member_mobile: memberMobile,
+        member_email: memberEmail,
+        occupation: occupation,
+        specialty: specialty,
+        company: company,
+        company_addressLine1: work_addressLine1,
+        company_addressLine2: work_addressLine2,
+        city: work_city,
+        emergency_contacts: emergency_contacts
+    });
+
+    const requestOptions = {
+        method: 'PUT',
+        headers: myHeader,
+        body: raw,
+        redirect: 'follow'
+    }
+    
+    return fetch(serverRoutes.updateSE, requestOptions);
+}
